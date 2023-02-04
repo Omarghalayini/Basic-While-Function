f4a4c3b91b15ebd86ddade2aaf13eb05d39f2be2
Omarghalayini <42498579+Omarghalayini@users.noreply.github.com>
2023-02-04 12:43:31 +0000

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,29 @@
+ #include <iostream>
+
+ using namespace std;
+
+ int main(){
+
+ int i = 1; //Define variable first
+
+ while (i <= 10){
+ cout << i << " ";
+++i;
+
+
+ }
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
